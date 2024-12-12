2a50cfe9a24f010f7fa70a7e5c6e29be55f77b77
Erik Carron <jecarron@gmail.com>
2024-12-12 00:03:57 -0600

    Added day 5

---
+++ Day05/part1.js
@@ -0,0 +1,54 @@
+let fs = require('fs')
+
+let parseInput = input => {
+  let lines = input.split('\r\n')
+  let rules = []
+  let updates = []
+
+  let rulePattern = /(\d+)\|(\d+)/mi
+  let i = 0
+
+  while (i < lines.length && lines[i] !== '') {
+    let matches = lines[i].match(rulePattern)
+    rules.push({ before: parseInt(matches[1]), after: parseInt(matches[2]) })
+    i++
+  }
+
+  i++
+
+  while (i < lines.length) {
+    updates.push(lines[i].split(',').map(v => parseInt(v)))
+    i++
+  }
+
+  return { rules: rules, updates: updates }
+}
+
+let isCorrectOrder = (update, rules) => {
+  let pages = {}
+
+  for (let i = 0; i < update.length; i++) {
+    pages[update[i].toString()] = i
+  }
+
+  for (let i = 0; i < rules.length; i++) {
+    let before = rules[i].before.toString()
+    let after = rules[i].after.toString()
+
+    if (pages.hasOwnProperty(before) && pages.hasOwnProperty(after)) {
+      if (pages[before] > pages[after]) {
+        return false
+      }
+    }
+  }
+
+  return true
+}
+
+let getMiddlePageNumber = update => update[Math.floor(update.length / 2)]
+
+let input = parseInput(fs.readFileSync('input.txt').toString())
+
+let answer = input.updates.filter(u => isCorrectOrder(u, input.rules)).reduce((accumulator, currentValue) => accumulator + getMiddlePageNumber(currentValue), 0)
+
+console.log(`Answer: ${answer}`)
